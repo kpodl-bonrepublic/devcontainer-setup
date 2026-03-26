c3f638d2483131027d9660ca0da44a00719fa216
Konrad Podloucky <konrad.podloucky@bonrepublic.com>
2026-03-26 16:47:27 +0100

    Add Kilo Shared Directory

--- setup/kilo.sh
+++ setup/kilo.sh
@@ -4,8 +4,11 @@ KILO_CONFIG_DIR=~/kilo/config
 TARGET_KILO_CONFIG_DIR=~/.config/kilo
 KILO_STATE_DIR=~/kilo/state
 TARGET_KILO_STATE_DIR=~/.local/state/kilo
+KILO_SHARE_DIR=~/kilo/share
+TARGET_KILO_SHARE_DIR=~/.local/share/kilo
 
 . $(dirname ${0})/common.lib
 ensure_directories_exists "${KILO_CONFIG_DIR}" "${KILO_STATE_DIR}"
 setup_link "${KILO_CONFIG_DIR}" "${TARGET_KILO_CONFIG_DIR}"
 setup_link "${KILO_STATE_DIR}" "${TARGET_KILO_STATE_DIR}"
+setup_link "${KILO_SHARE_DIR}" "${TARGET_KILO_SHARE_DIR}"
